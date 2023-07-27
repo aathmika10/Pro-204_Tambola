40bd9b996f1a35371089a49eb96ddfdc9bf357c2
aathmika10 <65343462+aathmika10@users.noreply.github.com>
2023-07-27 20:46:05 +0530

    Add files via upload

---
+++ pro-204/CLIENT/client.py
@@ -0,0 +1,74 @@
+import socket
+from threading import Thread
+from tkinter import *
+import random
+from PIL import ImageTk,Image
+
+screen_width=None
+screen_height=None
+
+SERVER=None
+PORT=None
+IP_ADDRESS=None
+
+canvas1=None
+
+playerName=None
+nameEntry=None
+nameWindow=None
+
+def saveName():
+    global SERVER
+    global playerName
+    global nameEntry
+    global nameWindow
+    playerName=nameEntry.get()
+    nameEntry.delete(0,END)
+    nameWindow.destroy()
+    SERVER.send(playerName.encode('utf-8'))
+
+
+def askPlayerName():
+    global playerName
+    global nameEntry
+    global nameWindow
+    global canvas1
+
+    nameWindow =Tk()
+    nameWindow.title("TAMBOLA")
+    nameWindow.geometry("800x600")
+    screen_width=nameWindow.winfo_screenwidth()
+    screen_height=nameWindow.winfo_screenheight()
+
+    bg=ImageTk.PhotoImage(file="./assets/background.png")
+
+    canvas1=Canvas(nameWindow,width=500,height=500)
+    canvas1.pack(fill="both",expand=True)
+
+    canvas1.create_image(0,0,image=bg,anchor="nw")
+    canvas1.create_text(screen_width/4,screen_height/8,text="Enter Name",font=("Chalkboard SE",50),fill="black")
+
+    nameEntry=Entry(nameWindow,width=15,justify="center",font=("Chalkboard SE",30),bd=5,bg="White")
+    nameEntry.place(x=screen_width/7,y=screen_height/5.5)
+
+    button=Button(nameWindow,text="Save",font=("Chalkboard SE",30),width=11,command=saveName,height=2,bg="#80deea",bd=3)
+    button.place(x=screen_width/6,y=screen_height/3)
+
+    nameWindow.resizable(True,True)
+    nameWindow.mainloop()
+
+
+def setup():
+    global SERVER
+    global PORT
+    global IP_ADDRESS
+
+    PORT=6000
+    IP_ADDRESS='127.0.0.1'
+
+    SERVER=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    SERVER.connect((IP_ADDRESS,PORT))
+
+    askPlayerName()
+
+setup()
---
+++ pro-204/CLIENT/server.py
@@ -0,0 +1,40 @@
+import socket
+from threading import Thread
+
+SERVER=None
+IP_ADDRESS='127.0.0.1'
+PORT=6000
+
+CLIENTS={}
+
+def acceptConnections():
+    global CLIENTS
+    global SERVER
+
+    while True:
+        player_socket,addr=SERVER.accept()
+        player_name=player_socket.recv(1024).decode().strip()
+        if(len(CLIENTS.keys())==0):
+            CLIENTS[player_name]={"player_type":"Player1"}
+        else:
+            CLIENTS[player_name]={"player_type":"Player2"}
+        
+        CLIENTS[player_name]["player_socket"]=player_socket
+        CLIENTS[player_name]["address"]=addr
+        CLIENTS[player_name]["player_name"]=player_name
+        CLIENTS[player_name]["Turn"]=False
+        print(f"Connection established with {player_name}:{addr}")
+
+def setup():
+    print("\n\t\t\t\t\t***Welcome to Tambola ***\n")
+
+    global SERVER
+    global PORT
+    global IP_ADDRESS
+
+    SERVER=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    SERVER.bind((IP_ADDRESS,PORT))
+
+    SERVER.listen(10)
+
+    print("Server is waiting for incoming connections...")
